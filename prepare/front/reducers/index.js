5117a416261960d9b7c5ed7bab524aae9aee9be6
TestJeong <jyunj3015@gmail.com>
2020-08-27 12:13:40 +0900

    리듀서 분리

--- prepare/front/reducers/index.js
+++ prepare/front/reducers/index.js
@@ -1,48 +1,22 @@
 import { HYDRATE } from "next-redux-wrapper";
+import { combineReducers } from "redux";
 
-const init = {
-  user: {
-    isLoggedIn: false,
-    user: null,
-    signUpdata: {},
-    loginData: {},
-  },
-  post: {
-    mainPosts: [],
-  },
-};
-
-export const loginAction = (data) => {
-  return {
-    type: "LOG_IN",
-    data,
-  };
-};
-
-export const logoutAction = () => {
-  return {
-    type: "LOG_OUT",
-  };
-};
+import user from "./user";
+import post from "./post";
 
 // (이전상태, 액션) => 다음상태를 만들어냄
-const rootReducer = (state = init, action) => {
-  switch (action.type) {
-    case HYDRATE:
-      return { ...state, ...action.payload };
-    case "LOG_IN":
-      return {
-        ...state,
-        user: { ...state.user, isLoggedIn: true, user: action.data },
-      };
-    case "LOG_OUT":
-      return {
-        ...state,
-        user: { ...state.user, isLoggedIn: false, user: null },
-      };
-    default:
-      return state;
-  }
-};
+const rootReducer = combineReducers({
+  index: (state = {}, action) => {
+    switch (action.type) {
+      case HYDRATE:
+        return { ...state, ...action.payload };
+
+      default:
+        return state;
+    }
+  }, // 서버사이드렌더링을 위해 HYDRATE를 추가하기 위해 switch문을 사용해 리듀서를 추가한다
+  user,
+  post,
+});
 
 export default rootReducer;
---
+++ prepare/front/reducers/post.js
@@ -0,0 +1,12 @@
+export const init = {
+  mainPosts: [],
+};
+
+const reducer = (state = init, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
