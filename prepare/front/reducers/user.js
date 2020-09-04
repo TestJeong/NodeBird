51ffe584983c9bd1e88857a98ff8a823f7798891
TestJeong <jyunj3015@gmail.com>
2020-09-04 22:35:52 +0900

    redux-saga 설정

--- prepare/front/reducers/user.js
+++ prepare/front/reducers/user.js
@@ -5,28 +5,52 @@ export const init = {
   loginData: {},
 };
 
-export const loginAction = (data) => {
+export const loginRequestAction = (data) => {
   return {
-    type: "LOG_IN",
+    type: "LOG_IN_REQUEST",
     data, // data : data 이므로 LoginForm에서 dispatch로 전달받은loginAction의 인자값이 data이다
   };
 };
 
-export const logoutAction = () => {
+export const loginSuccessAction = () => {
   return {
-    type: "LOG_OUT",
+    type: "LOG_IN_SUCCESS",
+  };
+};
+
+export const loginFailureAction = () => {
+  return {
+    type: "LOG_IN_FAILURE",
+  };
+};
+
+export const logoutRequestAction = () => {
+  return {
+    type: "LOG_OUT_REQUEST",
+  };
+};
+
+export const logoutSuccessAction = () => {
+  return {
+    type: "LOG_OUT_SUCCESS",
+  };
+};
+
+export const logoutFailureAction = () => {
+  return {
+    type: "LOG_OUT_FAILURE",
   };
 };
 
 const reducer = (state = init, action) => {
   switch (action.type) {
-    case "LOG_IN":
+    case "LOG_IN_REQUEST":
       return {
         ...state,
         isLoggedIn: true,
         me: action.data,
       };
-    case "LOG_OUT":
+    case "LOG_OUT_REQUEST":
       return {
         ...state,
         isLoggedIn: false,
--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,18 +1,25 @@
 import { createWrapper } from "next-redux-wrapper";
 import { createStore, compose, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
 
 import reducer from "../reducers";
 
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+  console.log(action);
+  return next(action);
+};
+
 const configureStore = () => {
   //개발용 배포용으로 만들어 놓음. 개발용일때는 히스토리가 쌓여 devTools를 사용할 수 있게 만듬
-  const middlewares = [];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, loggerMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
   const store = createStore(reducer, enhancer);
-
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
