7d18f20dc9450bcd44b850ebf6f5548d8b93f702
TestJeong <jyunj3015@gmail.com>
2020-10-31 23:22:29 +0900

    Modify : Nav ReDesign
    - 트위터 로고 추가
    - input창 위치 수정

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -2,6 +2,7 @@ import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import { Menu, Input, Row, Col } from "antd";
+import { TwitterOutlined } from "@ant-design/icons";
 import Router from "next/router";
 import LoginForm from "../components/LoginForm";
 import UserProfile from "../components/UserProfile";
@@ -9,13 +10,11 @@ import styled from "styled-components";
 import { useSelector } from "react-redux";
 import useInput from "../hooks/useInput";
 import FollowList from "./FollowList";
+import styles from "../styles//applayout.module.scss";
 
 const SearchInput = styled(Input.Search)`
   vertical-align: middle;
-`;
-
-const MenuAntd = styled(Menu)`
-  margin-bottom: 40px;
+  width: 94%;
 `;
 
 const AppLayout = ({ children }) => {
@@ -28,33 +27,32 @@ const AppLayout = ({ children }) => {
 
   return (
     <div style={{ padding: "10px", backgroundColor: "#f7f7f7" }}>
-      <MenuAntd mode="horizontal">
-        <Menu.Item>
+      <div className={styles.container}>
+        <nav className={styles.item}>
           <Link href="/">
-            <a>노드버드</a>
+            <a className={styles.item_aTag_logo}>
+              <TwitterOutlined />
+            </a>
           </Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link href="/profile">
-            <a>프로필</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item>
+        </nav>
+
+        <nav className={styles.item}>
           <SearchInput
             enterButton
             value={searchInput}
             onChange={onChangeSearchInput}
             onSearch={onSearch}
           />
-        </Menu.Item>
-        <Menu.Item>
-          <Link href="/signup">
-            <a>회원가입</a>
+        </nav>
+
+        <nav className={styles.item}>
+          <Link href="/profile">
+            <a className={styles.item_aTag_avatar}>프로필</a>
           </Link>
-        </Menu.Item>
-      </MenuAntd>
+        </nav>
+      </div>
       <Row gutter={0}>
-        <Col xs={24} md={6} style={{ backgroundColor: "#f7f7f7" }}>
+        <Col xs={24} md={6}>
           {me ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12} style={{ backgroundColor: "#f7f7f7" }}>
